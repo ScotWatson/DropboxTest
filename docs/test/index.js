bc076a9bfa363b2d3598a9978f8d0d7afc795871
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-02-24 06:40:31 -0500

    Create index.js

---
+++ docs/test/index.js
@@ -0,0 +1,86 @@
+/*
+(c) 2024 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const initPageTime = performance.now();
+
+const asyncWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncWindow ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+})();
+
+// Creates a GET Request to the specified endpoint
+function createRequestGET(endpoint, headers) {
+  return new self.Request(endpoint, {
+    method: "GET",
+    headers: headers,
+    mode: "cors",
+    credentials: "same-origin",
+    cache: "default",
+    redirect: "follow",
+    referrer: "about:client",
+    referrerPolicy: "",
+    integrity: "",
+    keepalive: "",
+    signal: null,
+    priority: "auto",
+  });
+}
+
+// Creates a POST Request to the specified endpoint
+function createRequestPOST(endpoint, body, headers) {
+  return new self.Request(endpoint, {
+    method: "POST",
+    headers: headers,
+    body: body,
+    mode: "cors",
+    credentials: "same-origin",
+    cache: "default",
+    redirect: "follow",
+    referrer: "about:client",
+    referrerPolicy: "",
+    integrity: "",
+    keepalive: "",
+    signal: null,
+    priority: "auto",
+  });
+}
+
+function start([ evtWindow ]) {
+  try {
+    const urlThis = new self.URL(window.location);
+    const paramsThis = urlThis.searchParams;
+    let strToken = paramsThis.get("token");
+    while (!strToken) {
+      strToken = window.prompt("Enter the access token: ");
+    }
+    (async function () {
+      const objReqBody = { limit: 1000 };
+      const jsonReqBody = JSON.stringify(objReqBody);
+      const blobReqBody = new Blob([ jsonReqBody ], { type: "application/json" });
+      const headers = [ [ "Authorization", "Bearer " + strToken ] ];
+      const reqFileList = createRequestPOST("https://api.dropboxapi.com/2/file_requests/list_v2", blobReqBody, headers);
+      const respFileList = await fetch(reqFileList);
+      console.log(respFileList);
+      if (respFileList.status === 200) {
+        const jsonRespBody = await respFileList.body.text();
+        const objRespBody = JSON.parse(jsonRespBody);
+        console.log(objRespBody);
+      }
+    })();
+  } catch (e) {
+    console.error(e);
+  }
+}
